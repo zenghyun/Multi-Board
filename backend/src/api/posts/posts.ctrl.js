6eca210e2198dc6bd74cc19903b5deec3536acd9
zenghyun <pipo36@naver.com>
2023-06-19 18:25:45 +0900

    Prepare REST API

---
+++ backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,191 @@
+import Post from '../../models/post';
+import mongoose from 'mongoose';
+import Joi from '@hapi/joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+  return next();
+};
+
+/* POST /api/posts 
+{
+  title: '제목',
+  body: '내용',
+  tags: ['태그1', '태그2']
+} 
+
+*/
+
+// export const write = async (ctx) => {
+//   const { title, body, tags } = ctx.request.body;
+//   const post = new Post({
+//     title,
+//     body,
+//     tags,
+//   });
+//   try {
+//     await post.save();
+//     ctx.body = post;
+//   } catch (e) {
+//     ctx.throw(500, e);
+//   }
+// };
+
+export const write = async (ctx) => {
+  const schema = Joi.object().keys({
+    // 객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), // required()가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
+  });
+
+  // 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/* 
+  GET /api/posts 
+*/
+export const list = async (ctx) => {
+  // query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
+  // 값이 주어지지 않았다면 1을 기본으로 사용합니다.
+  const page = parseInt(ctx.query.pate || '1', 10);
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-Page', Math.ceil(postCount / 10));
+    ctx.body = posts
+      .map((post) => ({
+        ...post,
+        body:
+          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+      }));
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/* 
+  GET /api/posts/:id
+*/
+
+export const read = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+  DELETE /api/posts/:id
+*/
+
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content ( 성공하기는 했지만 응답할 데이터 x)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: [
+      '수정', '태그'
+    ]
+  }
+*/
+
+// export const update = async (ctx) => {
+//   const { id } = ctx.params;
+//   try {
+//     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+//       new: true, // 업데이트 된 데이터를 반환
+//       // false 일 때는 업데이트되기 전의 데이터 반환
+//     }).exec();
+//     if (!post) {
+//       ctx.status = 404;
+//       return;
+//     }
+//     ctx.body = post;
+//   } catch (e) {
+//     ctx.throw(500, e);
+//   }
+// };
+
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  // 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 업데이트 된 데이터를 반환
+      // false 일 때는 업데이트되기 전의 데이터 반환
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
---
+++ backend/src/main.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+
+import api from './api';
+import createFakeData from './createFakeData';
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+// eslint-disable-next-line no-undef
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, {useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false, // 이 부분을 주석 처리하거나 제거합니다.
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+// PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
